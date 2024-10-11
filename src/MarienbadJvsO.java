534023aeaa947ad3b4504c6ee121bdff57308849
kamai7 <anaellecarre35@gmail.com>
2024-10-11 08:50:03 +0200

    modification des regles pour la version jvso

--- src/MarienbadJvsO.java
+++ src/MarienbadJvsO.java
@@ -65,7 +65,7 @@ class MarienbadJvsO {
 	void afficheRegles() {
 		System.out.println("jeu de");
 		System.out.println("\t\t __  __            _            _               _ \n\t\t|  \\/  |          (_)          | |             | |\n\t\t| \\  / | __ _ _ __ _  ___ _ __ | |__   __ _  __| |\n\t\t| |\\/| |/ _` | '__| |/ _ \\ '_ \\| '_ \\ / _` |/ _` |\n\t\t| |  | | (_| | |  | |  __/ | | | |_) | (_| | (_| |\n\t\t|_|  |_|\\__,_|_|  |_|\\___|_| |_|_.__/ \\__,_|\\__,_|");
-		System.out.println("\n * Version ordinateur * \n\nRegles du jeu: \n\n  *\tLe jeu est constitue de rangees d'allumettes\n  *\tChaque joueur prend chacun son tour le nombre d'allumette qu'il veut sur une seule rangee\n\t(donc etre 1 allumette et le nombre d'allumette qu'il y a sur la rangee)\n  *\tLe joueur qui prend les ou la derniere allumettes gagne");
+		System.out.println("\n * Version contre ordinateur * \n\nRegles du jeu: \n\n  *\tLe jeu est constitue de rangees d'allumettes\n  *\tLe joueur prend le nombre d'allumette qu'il veut sur une seule rangee\n\t(donc etre 1 allumette et le nombre d'allumette qu'il y a sur la rangee)\n  *\tSi le joueur prend la ou les derniere allumettes il gagne");
 		System.out.println("\n-----------------------------------------------------\n\t* Debut de la partie *\t\n-----------------------------------------------------\n\nMaintenant nous allons configurer les parametres de la partie:");
 	}
 	
